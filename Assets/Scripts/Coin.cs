7500e24e550cc169f81b4ade72d51d5d6fa203e5
Matthew O'Connell <oconnellomj@gmail.com>
2022-05-27 21:08:41 -0400

    backup for git

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+
+            if (player != null)
+            {
+                player.AddCoins();
+            }
+
+            Destroy(this.gameObject);
+        }
+
+    }
+}
